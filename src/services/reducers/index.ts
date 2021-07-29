293d1b7fcb2532a90aa314e187af380d7bba3a50
human77629 <human77629@oruzed.moe>
2021-07-29 21:10:51 +0300

    Rename reducers/index.js

---
+++ src/services/reducers/index.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import { burgerReducer } from "./burger";
+import { userReducer } from "./user";
+
+export const rootReducer = combineReducers({
+    burger: burgerReducer,
+    user: userReducer,
+})
